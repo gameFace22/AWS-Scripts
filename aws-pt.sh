de47ba1f391ee792d3c7fb2342b545efcff29b83
Apple <apple@Apples-MacBook-Pro.local>
2017-09-13 10:26:40 -0700

    Modified code structure

--- aws-pt.sh
+++ aws-pt.sh
@@ -4,31 +4,27 @@ echo "[*]Checking if AWS credentials are present"
 sleep 1
 
 if [[ -f ~/.aws/credentials ]] ; then
- echo "[+]Fetching Credentials" 
+ echo "[+]Fetching Credentials"
 else
  echo "[-]Please Configure AWS CLI"
- exit 
+ exit
 fi
 
 #Add test to check if the ~/.aws/config file is present
 
 response=$(curl --write-out %{http_code} --silent --output /dev/null s3.amazonaws.com/$1)
-if [ $response -eq 404 ] ; then 
+if [ $response -eq 404 ] ; then
  printf "\033[1;31m[-]Please check the bucket name. Bucket not found $1\033[0m\n"
  exit
 else
  if [ $response -eq 403 ] ; then
-  echo "[-]No Read Access"
-  printf "\033[1;31m[-]No Read Access to the bucket $1\033[0m\n"
-  exit
+  printf "\033[1;31m[-]No Read Access without Key\033[0m\n"
+  aws s3 ls s3://$1 > RESULTS
+  printf "\033[1;31m[+]Misconfigured ACL - Access with Any Key in $1\033[0m\n"
+  echo "[*]Check RESULTS to view the bucket contents"
+ else
+  aws s3 ls s3://$1 --no-sign-request > RESULTS
+  printf "\033[1;31m[+]Misconfigured ACL - Univeral Access in $1\033[0m\n"
+  echo "[*]Check RESULTS to view the bucket contents"
  fi
 fi
- 
-sleep 1
-touch aws-test.txt
-aws s3 ls s3://$1 > aws-test.txt
-printf "\033[1;31m[+]Misconfigured ACL in $1\033[0m\n"
-echo "[*]Files/Directories Found"
-cat aws-test.txt | awk '{print $4}' | sed -e 's/^/[+]/'
-rm aws-test.txt
-
