a43e153990be78c3ef76863995b33ebe2e7e7b9b
Apple <apple@Apples-MacBook-Pro.local>
2017-09-13 05:03:41 -0700

    Added Response Codes

--- aws-pt.sh
+++ aws-pt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #nobucketfound="bucket does not exist"
 
@@ -6,25 +6,24 @@ echo "[*]Checking if AWS credentials are present"
 sleep 1
 #Add test to check if the ~/.aws/config file is present
 
-#Add test to check if the bucket name is valid/present or not
-
-directorylisting()
-{
- echo "[*]Checking if AWS Bucket has directory listing enabled"
- sleep 1
- touch aws-test.txt
- aws s3 ls s3://$1 > aws-test.txt 
- count=$(wc -l aws-test.txt | awk '{print $1}')
- #echo $count
- if [ $count -gt 0 ] ; then
-  printf "\033[1;31m[+]Found Directory Listing in bucket $1\033[0m\n"
-  echo "[*]Files/Directories Found"
-  cat aws-test.txt | awk '{print $4}' | sed -e 's/^/[+]/'
-  rm aws-test.txt
- else 
-  printf "\033[1;31m[-]Directory Listing has been disabled in $1\033[0m\n"
+response=$(curl --write-out %{http_code} --silent --output /dev/null s3.amazonaws.com/$1)
+if [ $response -eq 404 ] ; then 
+ printf "\033[1;31m[-]Please check the bucket name. Bucket not found $1\033[0m\n"
+ exit
+else
+ if [ $response -eq 403 ] ; then
+  echo "[-]No Read Access"
+  printf "\033[1;31m[-]No Read Access to the bucket $1\033[0m\n"
+  exit
  fi
-}
-
-directorylisting
+fi
+ 
+#Add test to check if the bucket name is valid/present or not
+sleep 1
+touch aws-test.txt
+aws s3 ls s3://$1 > aws-test.txt
+printf "\033[1;31m[+]Misconfigured ACL in $1\033[0m\n"
+echo "[*]Files/Directories Found"
+cat aws-test.txt | awk '{print $4}' | sed -e 's/^/[+]/'
+rm aws-test.txt
 
